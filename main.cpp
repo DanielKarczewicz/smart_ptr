237eb2e05551ca9080dd7403607504e6abb45a1b
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-27 15:43:55 +0100

    added struct A and unique_ptr

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <memory>
 
+struct A {
+    int u;
+    A(int u) : u(u)  {std::cout << "ctor A\n";}
+    ~A() {std::cout << "dtor A\n";}
+};
+
+void f(std::unique_ptr<A> p) {
+    std::cout << p->u << '\n';
+}
+
 int main() {
-
-    std::cout << "hello there\n";
-
+    
+    auto ptr = std::unique_ptr<A>(new A(123));
+    
+    f(std::move(ptr));
 }
