25da58ec51840cadef083e30ec65180388359513
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-27 19:02:27 +0100

    added cyclic dep

--- cyclic_dep.cpp
+++ cyclic_dep.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <memory>
 
+/*
+        b_out ---> b1<--|
+                    |   |
+                    |   |
+                    v   |
+                    b2--|
+*/
 
 struct B {
-    B() : x(x) {}
+    B(int x) : x(x) {std::cout << "ctor of object no " << x << '\n';}
     int x;
     std::shared_ptr<B> next_obj;
+    ~B() {std::cout << "dtor of object no " << x << '\n';}
 };
 
 
@@ -14,4 +22,14 @@ int main() {
     auto b2 = std::make_shared<B>(2);
     b1->next_obj = b2;
     b2->next_obj = b1;
+    {
+        auto b_out = b1;
+        std::cout << "b1 count = " << b1.use_count() << '\n';
+    }
+    
+    
+
+
+    std::cout << "b1 count = " << b1.use_count() << '\n';
+    std::cout << "b2 count = " << b2.use_count() << '\n';
 }
